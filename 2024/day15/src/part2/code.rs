3febf0229f37ee97556011ab8dfe0bfefb8e37e6
MRdima98 <dimafrunza69@gmail.com>
2024-12-30 06:34:05 +0100

    Day 15 part 2 progress

--- 2024/day15/src/part2/code.rs
+++ 2024/day15/src/part2/code.rs
@@ -50,6 +50,12 @@ enum Direction {
     Right,
 }
 
+#[derive(Clone, Copy)]
+struct FishBox {
+    coord: Coord,
+    sym: char,
+}
+
 pub fn run(path: &str) {
     let input: String = fs::read_to_string(path).unwrap();
     let mut map = vec![];
@@ -79,12 +85,40 @@ pub fn run(path: &str) {
         }
     }
 
-    move_robot(&mut map, &moves, &mut robot_pos);
+    let mut wide_map = vec![];
+    for row in map.iter() {
+        let mut tmp = vec![];
+        for c in row.iter() {
+            if *c == '@' {
+                tmp.push('@');
+                tmp.push('.');
+            } else if *c == 'O' {
+                tmp.push('[');
+                tmp.push(']');
+            } else {
+                tmp.push(*c);
+                tmp.push(*c);
+            }
+        }
+        wide_map.push(tmp);
+    }
+
+    for (i, row) in wide_map.iter().enumerate() {
+        for (j, c) in row.iter().enumerate() {
+            if *c == '@' {
+                robot_pos = Coord(i as i64, j as i64);
+            }
+        }
+    }
+
+    pretty_print(&wide_map);
+
+    move_robot(&mut wide_map, &moves, &mut robot_pos);
 
     let mut sum = 0;
-    for (i, row) in map.iter().enumerate() {
+    for (i, row) in wide_map.iter().enumerate() {
         for (j, c) in row.iter().enumerate() {
-            if *c == 'O' {
+            if *c == '[' {
                 sum += j + i * 100;
             }
         }
@@ -117,7 +151,7 @@ fn move_robot(map: &mut [Vec<char>], moves: &[char], robot_pos: &mut Coord) {
         }
         //pretty_print(map);
         //map[robot_pos.0 as usize][robot_pos.1 as usize] = '@';
-        //pretty_print(map);
+        pretty_print(map);
         //thread::sleep(time::Duration::from_millis(250));
         //thread::sleep(time::Duration::from_millis(500));
         //print!(" {}[2J", 27 as char);
@@ -126,68 +160,125 @@ fn move_robot(map: &mut [Vec<char>], moves: &[char], robot_pos: &mut Coord) {
 
 fn valid_move(map: &mut [Vec<char>], robot_pos: &mut Coord, dir: Direction) {
     //println!("Robot before: {}", robot_pos);
-    let next_move: Coord;
+    let mut next_move: Coord;
     match dir {
-        Direction::Up => {
-            println!("Move up");
-            next_move = Coord(-1, 0)
-        }
-        Direction::Down => {
-            println!("Move down");
-            next_move = Coord(1, 0)
-        }
-        Direction::Left => {
-            println!("Move left");
-            next_move = Coord(0, -1)
-        }
-        Direction::Right => {
-            println!("Move right");
-            next_move = Coord(0, 1)
-        }
+        Direction::Up => next_move = Coord(-1, 0),
+        Direction::Down => next_move = Coord(1, 0),
+        Direction::Left => next_move = Coord(0, -1),
+        Direction::Right => next_move = Coord(0, 1),
     }
 
     let mut potential_move = *robot_pos + next_move;
     let mut tile = map[potential_move.0 as usize][potential_move.1 as usize];
 
-    if !(tile == '#' || tile == 'O') {
+    if !(tile == '#' || tile == '[' || tile == ']') {
         *robot_pos = potential_move;
     }
 
     let mut boxes = vec![];
-    if tile == 'O' {
-        boxes.push(potential_move);
-        loop {
-            potential_move = potential_move + next_move;
-            tile = map[potential_move.0 as usize][potential_move.1 as usize];
+    if tile == '[' || tile == ']' {
+        boxes = bfs(map, potential_move);
 
-            if tile == 'O' {
-                boxes.push(potential_move);
+        if next_move.0 == 0 {
+            let mut clean_box = vec![];
+            for i in 0..boxes.len() {
+                if boxes[i].coord.0 == robot_pos.0 {
+                    clean_box.push(boxes[i]);
+                } else {
+                    map[boxes[i].coord.0 as usize][boxes[i].coord.1 as usize] = boxes[i].sym;
+                }
             }
 
-            if tile == '#' {
-                boxes = vec![];
-                break;
+            boxes = clean_box;
+        } else if next_move.0 == 1 {
+            println!("Moving down");
+            let mut clean_box = vec![];
+            for i in 0..boxes.len() {
+                if boxes[i].coord.0 > robot_pos.0 {
+                    clean_box.push(boxes[i]);
+                } else {
+                    map[boxes[i].coord.0 as usize][boxes[i].coord.1 as usize] = boxes[i].sym;
+                }
             }
 
-            if tile == '.' {
-                break;
+            boxes = clean_box;
+        } else if next_move.0 == -1 {
+            println!("Moving up");
+            println!("Robo pos: {robot_pos}");
+            let mut clean_box = vec![];
+            for i in 0..boxes.len() {
+                if boxes[i].coord.0 < robot_pos.0 {
+                    clean_box.push(boxes[i]);
+                } else {
+                    map[boxes[i].coord.0 as usize][boxes[i].coord.1 as usize] = boxes[i].sym;
+                }
             }
+
+            boxes = clean_box;
         }
 
-        //println!("Boxes: {:?}", boxes);
+        for ele in boxes.iter() {
+            print!("{} ", ele.coord);
+        }
+        println!();
+
+        let valid = check_walls(map, boxes.clone(), next_move);
+
         for el in boxes.iter() {
-            //map[el.0 as usize][el.1 as usize] = '.';
-            let tmp = *el + next_move;
-            potential_move -= next_move;
-            map[tmp.0 as usize][tmp.1 as usize] = 'O';
-            //println!("tmp: {:?}", tmp);
+            if !valid {
+                next_move = Coord(0, 0);
+            }
+            let tmp = el.coord + next_move;
+            map[tmp.0 as usize][tmp.1 as usize] = el.sym;
+        }
+
+        if !valid {
+            boxes = vec![];
         }
 
         if !boxes.is_empty() {
+            println!("Potential move: {potential_move}");
             *robot_pos = potential_move;
         }
     }
     //println!("Robot after: {}", robot_pos);
+}
+
+fn check_walls(map: &mut [Vec<char>], boxes: Vec<FishBox>, dir: Coord) -> bool {
+    match dir {
+        Coord(0, 1) => {
+            for el in boxes {
+                if map[el.coord.0 as usize][(el.coord.1 + 1) as usize] == '#' {
+                    return false;
+                }
+            }
+            true
+        }
+        Coord(0, -1) => {
+            for el in boxes {
+                if map[el.coord.0 as usize][(el.coord.1 - 1) as usize] == '#' {
+                    return false;
+                }
+            }
+            true
+        }
+        Coord(1, 0) => {
+            for el in boxes {
+                if map[(el.coord.0 + 1) as usize][el.coord.1 as usize] == '#' {
+                    return false;
+                }
+            }
+            true
+        }
+        _ => {
+            for el in boxes {
+                if map[(el.coord.0 - 1) as usize][el.coord.1 as usize] == '#' {
+                    return false;
+                }
+            }
+            true
+        }
+    }
 }
 
 fn pretty_print(map: &[Vec<char>]) {
@@ -199,3 +290,106 @@ fn pretty_print(map: &[Vec<char>]) {
     }
     println!();
 }
+
+// TODO
+// You need a specific kind of bfs, two actually
+// one that check ONLY above head
+// one that check only rows
+fn bfs(map: &mut [Vec<char>], start: Coord) -> Vec<FishBox> {
+    let mut queue = vec![];
+    let mut fishBox = vec![];
+
+    queue.push(start);
+    fishBox.push(FishBox {
+        coord: start,
+        sym: map[start.0 as usize][start.1 as usize],
+    });
+    map[start.0 as usize][start.1 as usize] = '.';
+
+    while !queue.is_empty() {
+        let curr = queue.pop().unwrap();
+        let neighbours = get_neighbour(map, curr);
+
+        for neigh in neighbours.iter() {
+            if !(map[neigh.0 as usize][neigh.1 as usize] == '.') {
+                queue.push(*neigh);
+                fishBox.push(FishBox {
+                    coord: *neigh,
+                    sym: map[neigh.0 as usize][neigh.1 as usize],
+                });
+                map[neigh.0 as usize][neigh.1 as usize] = '.';
+            }
+        }
+    }
+
+    fishBox
+}
+
+fn get_neighbour(map: &mut [Vec<char>], node: Coord) -> Vec<Coord> {
+    let mut pot_neighbours = vec![];
+    pot_neighbours.push(node - Coord(-1, 0));
+    pot_neighbours.push(node - Coord(-1, -1));
+    pot_neighbours.push(node - Coord(-1, 1));
+    pot_neighbours.push(node - Coord(1, 0));
+    pot_neighbours.push(node - Coord(1, -1));
+    pot_neighbours.push(node - Coord(1, 1));
+    pot_neighbours.push(node - Coord(0, -1));
+    pot_neighbours.push(node - Coord(0, 1));
+    let mut neighbours = vec![];
+
+    for neigh in pot_neighbours {
+        let tmp = map[neigh.0 as usize][neigh.1 as usize];
+        if tmp == '[' || tmp == ']' {
+            neighbours.push(neigh)
+        }
+    }
+
+    neighbours
+}
+
+//boxes.push(FishBox {
+//    coord: potential_move,
+//    sym: map[potential_move.0 as usize][potential_move.1 as usize],
+//});
+//boxes.push(FishBox {
+//    coord: potential_move + next_move,
+//    sym: map[(potential_move.0 + next_move.0) as usize]
+//        [(potential_move.1 + next_move.1) as usize],
+//});
+//loop {
+//    potential_move = potential_move + next_move + next_move;
+//    tile = map[potential_move.0 as usize][potential_move.1 as usize];
+//
+//    if tile == '[' {
+//        boxes.push(FishBox {
+//            coord: potential_move,
+//            sym: map[potential_move.0 as usize][potential_move.1 as usize],
+//        });
+//        boxes.push(FishBox {
+//            coord: potential_move + next_move,
+//            sym: map[(potential_move.0 + next_move.0) as usize]
+//                [(potential_move.1 + next_move.1) as usize],
+//        });
+//    }
+//
+//    if tile == ']' {
+//        boxes.push(FishBox {
+//            coord: potential_move,
+//            sym: map[potential_move.0 as usize][potential_move.1 as usize],
+//        });
+//        boxes.push(FishBox {
+//            coord: potential_move + next_move,
+//            sym: map[(potential_move.0 + next_move.0) as usize]
+//                [(potential_move.1 + next_move.1) as usize],
+//        });
+//    }
+//
+//    if tile == '#' {
+//        boxes = vec![];
+//        break;
+//    }
+//
+//    if tile == '.' {
+//        break;
+//    }
+//}
